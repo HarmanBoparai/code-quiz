80d43256bdd7d5b166cf04682f7e4ce677c1146c
HarmanBoparai <erharmanboparai@gmail.com>
2022-10-20 22:35:22 -0400

    written javascript functions

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let startScreen = document.querySelector(".start-screen");
 let startButton = document.getElementById("start-button");
 let questionCount;
 let scoreCount = 0;
-let count = 70;
+let count = 10;
 let countdown;
 
 //questions array
@@ -36,9 +36,9 @@ const quizArray = [
       },
       {
         id: "3",
-        question: " Which of the following is the paragraph tag in HTML?",
-        options: ["<p>", "<b>", "<pre>", "None of the above"],
-        correct: "<p>",
+        question: "Which of the following are the attributes of the tag?",
+        options: ["method", "action", "Both (a) & (b)", "None of the above"],
+        correct: "Both (a) & (b)",
       },
       {
         id: "4",
@@ -48,3 +48,134 @@ const quizArray = [
       },
     ];
     
+    //Restart Quiz
+restart.addEventListener("click", () => {
+    initial();
+    displayContainer.classList.remove("hide");
+    scoreContainer.classList.add("hide");
+  });
+
+  nextBtn.addEventListener(
+    "click",
+    (displayNext = () => {
+      //increment questionCount
+      questionCount += 1;
+      //if last question
+      if (questionCount == quizArray.length) {
+        //hide question container and display score
+        displayContainer.classList.add("hide");
+        scoreContainer.classList.remove("hide");
+        //user score
+        userScore.innerHTML =
+          "Your score is " + scoreCount + " out of " + questionCount;
+      } else {
+        //display questionCount
+        countOfQuestion.innerHTML =
+          questionCount + 1 + " of " + quizArray.length + " Question";
+        //display quiz
+        quizDisplay(questionCount);
+        count = 11;
+        clearInterval(countdown);
+        timerDisplay();
+      }
+    })
+  );
+
+  //Setting timer
+  const timerDisplay = () => {
+    countdown = setInterval(() => {
+      count--;
+      timeLeft.innerHTML = `${count}s`;
+      if (count == 0) {
+        clearInterval(countdown);
+        displayNext();
+      }
+    }, 1000);
+  };
+
+  //Display quiz
+const quizDisplay = (questionCount) => {
+    let quizCards = document.querySelectorAll(".container-mid");
+    quizCards.forEach((card) => {
+        card.classList.add("hide"); //Hides other questions
+      });
+      //display current question card
+  quizCards[questionCount].classList.remove("hide");
+};
+
+function quizCreator() {
+    quizArray.sort(() => Math.random() - 0.5);
+  for (let i of quizArray) {
+    i.options.sort(() => Math.random() - 0.5);
+    let div = document.createElement("div");
+    div.classList.add("container-mid", "hide");
+    countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+    let question_DIV = document.createElement("p");
+    question_DIV.classList.add("question");
+    question_DIV.innerHTML = i.question;
+    div.appendChild(question_DIV);
+
+    //options
+    div.innerHTML += `
+    <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+     <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+      <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+       <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+    `;
+    quizContainer.appendChild(div);
+  }
+}
+//Checker Function to check if option is correct or not
+function checker(userOption) {
+    let userSolution = userOption.innerText;
+    let question =
+      document.getElementsByClassName("container-mid")[questionCount];
+    let options = question.querySelectorAll(".option-div");
+  
+    //if user clicked answer == correct option stored in object
+    if (userSolution === quizArray[questionCount].correct) {
+      userOption.classList.add("correct");
+      scoreCount++;
+    } else {
+      userOption.classList.add("incorrect");
+      //For marking the correct option
+      options.forEach((element) => {
+        if (element.innerText == quizArray[questionCount].correct) {
+          element.classList.add("correct");
+        }
+      });
+    }
+  
+    //clear interval(stop timer)
+    clearInterval(countdown);
+    //disable all options
+    options.forEach((element) => {
+      element.disabled = true;
+    });
+  }
+  
+  //initial setup
+  function initial() {
+    quizContainer.innerHTML = "";
+    questionCount = 0;
+    scoreCount = 0;
+    count = 11;
+    clearInterval(countdown);
+    timerDisplay();
+    quizCreator();
+    quizDisplay(questionCount);
+  }
+  
+  //when user click on start button
+  startButton.addEventListener("click", () => {
+    startScreen.classList.add("hide");
+    displayContainer.classList.remove("hide");
+    initial();
+  });
+  
+  //hide quiz and display start screen
+  window.onload = () => {
+    startScreen.classList.remove("hide");
+    displayContainer.classList.add("hide");
+  };
+  
